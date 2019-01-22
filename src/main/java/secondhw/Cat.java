522d93307ea413e94dfeaeecf163ead4a343d2c6
natailchenko <n.ilchenko90@gmail.com>
2019-01-22 22:18:05 +0200

    Cat homework

--- src/main/java/secondhw/Cat.java
+++ src/main/java/secondhw/Cat.java
@@ -1,26 +1,28 @@
-package secondhw.task1;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+package secondhw;
+import java.util.*;
+
 public class Cat {
     public static void main(String[] args) {
 
-        HashMap<String, String> hashMap = new HashMap<String, String>;
-        hashMap.put("Cat1","мурка");
-        hashMap.put("Cat2","васька");
-        hashMap.put("Cat3","кузя");
-        hashMap.put("Cat4","жужа");
-        hashMap.put("Cat5","лелик");
-        hashMap.put("Cat6","кокс");
-        hashMap.put("Cat7","степа");
-        hashMap.put("Cat8","машка");
-        hashMap.put("Cat9","дашка");
-        hashMap.put("Cat10","люська");
 
-      Iterator it = mp.entrySet().iterator();
-    while (it.hasNext()) {
-        Map.Entry pair = (Map.Entry)it.next();
-        System.out.println(pair.getKey() + " = " + pair.getValue());
-        it.remove();
+        HashMap<String, String> myhashMap = new HashMap<String, String>();
+        myhashMap.put("Cat1","мурка");
+        myhashMap.put("Cat2","васька");
+        myhashMap.put("Cat3","кузя");
+        myhashMap.put("Cat4","жужа");
+        myhashMap.put("Cat5","лелик");
+        myhashMap.put("Cat6","кокс");
+        myhashMap.put("Cat7","степа");
+        myhashMap.put("Cat8","машка");
+        myhashMap.put("Cat9","дашка");
+        myhashMap.put("Cat10","люська");
+
+        Set entrySet = myhashMap.entrySet();
+        Iterator it = entrySet.iterator();
+
+        while(it.hasNext()){
+            Map.Entry me = (Map.Entry)it.next();
+            System.out.println(me.getKey() + ": " +me.getValue());
+        }
     }
 }
